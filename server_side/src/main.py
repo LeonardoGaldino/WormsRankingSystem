6791b60500b53c7305a86f8890fa7e91f63c74fb
Leonardo Galdino <lcgm@cin.ufpe.br>
2020-06-03 23:12:57 -0300

    Add CORS support

--- server_side/src/main.py
+++ server_side/src/main.py
@@ -2,10 +2,12 @@ import json
 from functools import reduce
 
 from flask import Flask, request, Response
+from flask_cors import CORS
 import psycopg2
 
 
 app = Flask(__name__)
+CORS(app)
 conn = psycopg2.connect("dbname=worms user=lcgm password=123k321k")
 cursor = conn.cursor()
 
