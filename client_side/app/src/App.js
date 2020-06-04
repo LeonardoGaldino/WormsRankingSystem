97b92c299cf3ced069b39cae0691999c4932605f
Leonardo Galdino <lcgm@cin.ufpe.br>
2020-06-04 13:46:19 -0300

    Finishes implementing games section

--- client_side/app/src/App.js
+++ client_side/app/src/App.js
@@ -9,6 +9,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import StarIcon from '@material-ui/icons/Star';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 // TODO: use dotenv and webpack to correctly set environment parameters
 const API_URL = 'http://localhost';
@@ -79,10 +81,16 @@ class RankingTable extends React.Component {
                   <TableRow key={row.name}>
                     <RankingNumberTableCell name={row.name} idx={idx}></RankingNumberTableCell>
                     <TableCell align="center" component="td" scope="row">{row.name}</TableCell>
-                    <TableCell align="center" component="td" scope="row">{row.ranking.toFixed(2)}</TableCell>
+                    <TableCell style={{color: row.ranking >= 0 ? 'green' : 'red'}} 
+                      align="center" component="td" scope="row">
+                        {row.ranking.toFixed(2)}
+                    </TableCell>
                     <TableCell align="center" component="td" scope="row">{row.games}</TableCell>
                     <TableCell align="center" component="td" scope="row">{row.wins}</TableCell>
-                    <TableCell align="center" component="td" scope="row">{row.score_avg.toFixed(2)}</TableCell>
+                    <TableCell style={{color: row.score_avg >= 0 ? 'green' : 'red'}} 
+                      align="center" component="td" scope="row">
+                        {row.score_avg.toFixed(2)}
+                    </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
@@ -112,14 +120,55 @@ class Games extends React.Component {
   }
 
   render() {
-    return <h1>Hello, world from games</h1>;
+    return Object.keys(this.state.games).map(gameDate => {
+      return <div key={gameDate+'div'}>
+        <h3>{gameDate}</h3>
+        {Object.keys(this.state.games[gameDate]).map(gameId => 
+          <Game key={gameId} playerEntries={this.state.games[gameDate][gameId]}></Game>
+        )}
+      </div>;
+    });
   }
 }
 
 class Game extends React.Component {
 
   render() {
-    return <h1>Hello, world from game</h1>;
+    return <MuiTableContainer style={{marginTop: 10}} component={Paper}>
+      <Table size='medium' aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center"><strong>Posição</strong></TableCell>
+            <TableCell align="center"><strong>Nome</strong></TableCell>
+            <TableCell align="center"><strong>Kills</strong></TableCell>
+            <TableCell align="center"><strong>Suicídios</strong></TableCell>
+            <TableCell align="center"><strong>Score</strong></TableCell>
+            <TableCell align="center"><strong>&Delta;Ranking</strong></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {this.props.playerEntries.map((entry, idx) => (
+            <TableRow key={entry.name}>
+              <TableCell align="center" component="td" scope="entry">{entry.position}°</TableCell>
+              <TableCell align="center" component="td" scope="entry">{entry.name}</TableCell>
+              <TableCell align="center" component="td" scope="entry">{entry.kills}</TableCell>
+              <TableCell align="center" component="td" scope="entry">{entry.suicides}</TableCell>
+              <TableCell align="center" component="td" scope="entry">{entry.score}</TableCell>
+              <TableCell align="center" component="td" scope="entry">
+                {parseFloat(entry.ranking_delta) !== 0.0 && (entry.ranking_delta > 0 ? 
+                <ArrowUpwardIcon style={{marginRight: 3, position: 'relative', top: 6, color: 'green'}}>
+                </ArrowUpwardIcon> 
+                  : 
+                <ArrowDownwardIcon style={{marginRight: 3, position: 'relative', top: 6, color: 'red'}}>
+                </ArrowDownwardIcon>)}
+                {Math.abs(entry.ranking_delta) < 10.0 && 0}
+                {Math.abs(entry.ranking_delta).toFixed(2)}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </MuiTableContainer>;
   }
 }
 
