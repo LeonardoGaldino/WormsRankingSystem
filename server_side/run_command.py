f4e44ffc795e6fefba39247bb22b5dcf21ae4f7f
Leonardo Galdino <lcgm@cin.ufpe.br>
2020-06-22 21:32:35 -0300

    Specify command name options when command not found

--- server_side/run_command.py
+++ server_side/run_command.py
@@ -99,14 +99,16 @@ VALID_COMMANDS: [Command] = [
 ]
 
 def main():
+    valid_commands_name = [command.command_name() for command in VALID_COMMANDS] 
     if len(argv) < 2:
         print('Specify the command name to be ran. No command specified. Should be one of {}'
-            .format([command.command_name() for command in VALID_COMMANDS]), file=stderr)
+            .format(valid_commands_name), file=stderr)
         exit(1)
     try:
         command_type = filter(lambda command: command.command_name() == argv[1], VALID_COMMANDS).__next__()
     except StopIteration:
-        print('No command with name {} found.'.format(argv[1]), file=stderr)
+        print('No command with name {} found. Should be one of {}'
+            .format(argv[1], valid_commands_name), file=stderr)
         exit(1)
 
     command = command_type(argv[2:])
