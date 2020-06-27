acac8295d3ab2198ee4fe7b1bd3ffdb60c767868
Leonardo Galdino <lcgm@cin.ufpe.br>
2020-06-26 22:58:46 -0300

    Implements help section modal

--- client_side/app/src/components/HelpSection.js
+++ client_side/app/src/components/HelpSection.js
@@ -1,12 +1,69 @@
 import React from 'react';
 import HelpIcon from '@material-ui/icons/Help';
-
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import Dialog from '@material-ui/core/Dialog';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 
 class HelpSection extends React.Component {
+    state = {
+        helpModalOpen: false,
+    }
+    
+    dialogContentRef = React.createRef();
+
+    changeModal(openState) {
+        this.setState({
+            helpModalOpen: openState,
+        });
+    }
+
+    typeSetEquations() {
+        window.MathJax.typeset([this.dialogContentRef.current]);
+    }
+
     render() {
-        return <div className="help-icon-div">
-                <HelpIcon color="inherit" fontSize="inherit" onClick={() => alert('Help!')}></HelpIcon>
-            </div>;
+        return <>
+                <div className="help-icon-div">
+                    <HelpIcon color="inherit" fontSize="inherit" onClick={() => this.changeModal(true)}></HelpIcon>
+                </div>
+
+                <Dialog
+                maxWidth='md'
+                fullWidth={true}
+                open={this.state.helpModalOpen}
+                onClose={() => this.changeModal(false)}
+                onEntered={this.typeSetEquations.bind(this)}>
+                    <DialogTitle><strong>Worms Ranking System</strong></DialogTitle>
+    
+                    <DialogContent ref={this.dialogContentRef}>
+                        <h4>This is a ranking system built for Worms Armageddon v3.7.2.1.</h4>
+                        <p>The data here displayed is collected by a program that reads through the game process' memory and is then sent to the server.</p>
+                        <p>The server, in turn, computes each player score and ranking change and saves everything into a database.</p>
+                        <p className="help-section-paragraph"> <strong> Score measures how well a player did in a given game and is calculated as: </strong> </p>
+                        {"\\[ score = { 3 * \\text{ } kills + { damage \\over 10 } - { self damage \\over 10 } } \\]"}
+                        <p className="help-section-paragraph"> <strong> Δranking is the amount of change in a player's ranking after a given game and is calculated as: </strong> </p>
+                        {"\\[ Δranking = { (score_{player} - score_{game}) * \\text{ } \\Big({Ranking_{player} \\over Ranking_{game}}\\Big)^2} \\]"}
+                        <div style={{textAlign: 'center', marginTop: 10, marginBottom: 10}}>
+                            <p className="help-section-paragraph">
+                                <strong>where:</strong>
+                            </p>
+                        </div>
+                        {"\\[ score_{game} = \\text{average score of all players in game} \\]"}
+                        {"\\[ ranking_{game} = \\text{average ranking of all players in game} \\]"}
+                    </DialogContent>
+    
+                    <Divider />
+                    
+                    <DialogActions>
+                        <Button onClick={() => this.changeModal(false)} color="primary" autoFocus>
+                            Close
+                        </Button>
+                    </DialogActions>
+              </Dialog>
+            </>;
     }
 }
 
