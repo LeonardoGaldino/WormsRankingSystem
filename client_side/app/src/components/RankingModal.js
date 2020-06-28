a32a7bf4f3c56b7e9d4977e0fd74e88318d99c23
Leonardo Galdino <lcgm@cin.ufpe.br>
2020-06-27 23:37:37 -0300

    Add info for clicking a point

--- client_side/app/src/components/RankingModal.js
+++ client_side/app/src/components/RankingModal.js
@@ -7,6 +7,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
 import Dialog from '@material-ui/core/Dialog';
 import Divider from '@material-ui/core/Divider';
+import InfoIcon from '@material-ui/icons/Info';
 
 class RankingModal extends React.Component {
 
@@ -26,6 +27,10 @@ class RankingModal extends React.Component {
                     <DialogTitle id="alert-dialog-title">{this.props.name}'s ranking history</DialogTitle>
     
                     <DialogContent>
+                        <p style={{marginBottom: 8}}> 
+                            <InfoIcon style={{position: 'relative', top: 4}} color={'primary'} fontSize={'small'}></InfoIcon> Click on a point to see game details 
+                        </p>
+
                         <RankingHistoryChart player={this.props.name}></RankingHistoryChart>
                     </DialogContent>
     
