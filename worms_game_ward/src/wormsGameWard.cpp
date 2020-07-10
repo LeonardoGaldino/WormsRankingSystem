d2b73674efa8a04a667e8adf1315ce33ec7734a3
LeonardoGaldino <lcgm@cin.ufpe.br>
2020-07-10 15:26:22 -0300

    Add thread for controlling upload game data or not

--- worms_game_ward/src/wormsGameWard.cpp
+++ worms_game_ward/src/wormsGameWard.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 #include <windows.h>
+#include <conio.h>
 
 #include "worms.cpp"
 
 using namespace std;
 
+bool shouldUploadData;
+
+DWORD WINAPI readShouldUploadDataControlFlag(LPVOID args) {
+    bool* v = (bool*) args;
+
+    while(true) {
+        char c = _getch();
+        switch(c) {
+            case 's':
+            case 'S':
+                *v = true;
+                break;
+            case 'n':
+            case 'N':
+                *v = false;
+                break;
+        }
+        cout << "Reading new input in 1s (This is the time to kill the process if needed)" << endl;
+        Sleep(1000);
+    }
+    return 0x0;
+}
+
 int main() {
+    shouldUploadData = true;
+    CreateThread(NULL, 0, readShouldUploadDataControlFlag, &shouldUploadData, 0, NULL);
     while(true) {
         HANDLE hProcess = attachToProcess();
         if(hProcess == NULL){
@@ -14,7 +40,7 @@ int main() {
             continue;
         }
 
-        WormsGame game = WormsGame(hProcess, 1000);
+        WormsGame game = WormsGame(hProcess, &shouldUploadData, 1000);
 
         game.watchGame();
     }
