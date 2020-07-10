5c2d96cdb575da677ba4cc5dcdcdb7528bafa5da
Leonardo Galdino <lcgm@cin.ufpe.br>
2020-07-10 00:04:26 -0300

    Add build folder because raspberry pi cant build it (not enough RAM)

---
+++ client_side/app/build/precache-manifest.d9dfd3cd522af3c4525a5ccc9d5bd1c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820ec6144a6b58dfa7f413cd032d8df8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "66a751f7e1824c957057",
+    "url": "./static/css/main.ad7ebc58.chunk.css"
+  },
+  {
+    "revision": "f904cd20ce3deb7a18cd",
+    "url": "./static/js/2.85a70023.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "./static/js/2.85a70023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a751f7e1824c957057",
+    "url": "./static/js/main.cb8a88f4.chunk.js"
+  },
+  {
+    "revision": "628eaaaaf82a98dd9226",
+    "url": "./static/js/runtime-main.a708e874.js"
+  }
+]);
