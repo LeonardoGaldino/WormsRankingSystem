c9235df0db7564e9682a3897c105bc8a7fd7ebe9
LeonardoGaldino <lcgm@cin.ufpe.br>
2020-07-17 19:22:09 -0300

    Make server endpoint available from environment variable instead of hardcoded

--- worms_game_ward/src/save_game_data.py
+++ worms_game_ward/src/save_game_data.py
@@ -1,5 +1,5 @@
 from sys import argv, exit
-from os import remove
+from os import remove, environ
 import json
 
 import requests
@@ -11,7 +11,16 @@ def check_file_name_argv():
             err_file.write("Expected 1: game_data file name, but received {}\n\n".format(str(argv)))
         exit(1)
 
-def save_game(file_name):
+def get_server_endpoint_env_var():
+    worms_server_endpoint = environ.get('WORMS_SERVER_ENDPOINT', None)
+    if worms_server_endpoint is None:
+        with open('py_err', 'a+') as err_file:
+            err_file.write("WORMS_SERVER_ENDPOINT Environment variable not set.\n")
+        exit(1)
+
+    return worms_server_endpoint
+
+def save_game(file_name, worms_server_endpoint):
     with open(file_name, "r") as file:
         data = file.readlines()
         if len(data[:-1]) == 0:
@@ -33,7 +42,7 @@ def save_game(file_name):
                     }, teams_data))
                 }
 
-                response = requests.post("http://192.168.0.76:17015/worms/api/create/game", json=payload)
+                response = requests.post("http://{}/worms/api/create/game".format(worms_server_endpoint), json=payload)
                 if response.status_code != 200:
                     with open('py_err', 'a+') as err_file:
                         err_file.write("Server failed on file {}, status code: {}\n\n".format(file_name, response.status_code))
@@ -47,4 +56,5 @@ def save_game(file_name):
 
 if __name__ == '__main__':
     check_file_name_argv()
-    save_game(argv[1])
+    worms_server_endpoint = get_server_endpoint_env_var()
+    save_game(argv[1], worms_server_endpoint)
